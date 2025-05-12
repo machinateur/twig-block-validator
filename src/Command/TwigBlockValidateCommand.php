dc00f23446ea29cc86304e3d41ed53dff0e267f6
machinateur <hello@machinateur.dev>
2025-05-12 23:13:21 +0200

    Remove import

--- src/Command/TwigBlockValidateCommand.php
+++ src/Command/TwigBlockValidateCommand.php
@@ -33,7 +33,6 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Style\OutputStyle;
 use Twig\Loader\FilesystemLoader;
 
 /**
