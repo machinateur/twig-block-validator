26f09fe779204d71107713aaa905cc735d451c6d
machinateur <hello@machinateur.dev>
2025-05-28 01:59:35 +0200

    Add example patcher

--- scoper.inc.php
+++ scoper.inc.php
@@ -6,4 +6,22 @@ return [
     'prefix'          => 'Isolated',
     'exclude-classes' => [\Composer\InstalledVersions::class],
     'exclude-files'   => ['vendor/composer/InstalledVersions.php'],
+    'patchers'        => [
+        static function (string $filePath, string $prefix, string $content): string {
+            //
+            // PHP-Parser patch for yaml/yml files (redundant).
+            //
+            //if (\str_ends_with($filePath, '.yaml')
+            //    || \str_ends_with($filePath, '.yml')
+            //) {
+            //    return \preg_replace(
+            //        "%Machinateur\\\\TwigBlockValidator\\\\%",
+            //        $prefix . '\\Machinateur\\TwigBlockValidator\\',
+            //        $content,
+            //    );
+            //}
+
+            return $content;
+        },
+    ],
 ];
