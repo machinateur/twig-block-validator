eb8cd601b96a5e1fa62dc3320533550fe72a68dd
machinateur <hello@machinateur.dev>
2025-06-03 00:08:34 +0200

    Remove old bundles

--- config/bundles.php
+++ config/bundles.php
@@ -10,17 +10,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class                    => ['all' => true],
     Symfony\Bundle\DebugBundle\DebugBundle::class                  => ['dev' => true, 'test' => true],
 
-    Shopware\Core\Profiling\Profiling::class                       => ['dev' => true],
-    Shopware\Core\Framework\Framework::class                       => ['dev' => true],
-    Shopware\Core\System\System::class                             => ['dev' => true],
-    Shopware\Core\Content\Content::class                           => ['dev' => true],
-    Shopware\Core\Checkout\Checkout::class                         => ['dev' => true],
-    Shopware\Core\DevOps\DevOps::class                             => ['dev' => true],
-    Shopware\Core\Maintenance\Maintenance::class                   => ['dev' => true],
-    Shopware\Administration\Administration::class                  => ['dev' => true],
-    Shopware\Storefront\Storefront::class                          => ['dev' => true],
-    //Shopware\Elasticsearch\Elasticsearch::class                    => ['dev' => true],
-    //Shopware\Core\Service\Service::class                           => ['dev' => true],
-
     Machinateur\TwigBlockValidator\TwigBlockValidatorBundle::class => ['dev' => true, 'test' => true],
 ];
